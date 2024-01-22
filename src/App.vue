503905c957b11f29501b3dfe24c780c2f47a069e
CappelliLeonardo <l.cappelli1995@gmail.com>
2024-01-22 15:14:12 +0100

    add bootstrap & axios

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import axios from 'axios';
 
 export default {
     data() {
@@ -16,6 +17,12 @@ export default {
     },  
     methods: {
 
+    },
+
+    mounted(){
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then((response)=>{
+            console.log(response)
+        })
     }
 }
 </script>
